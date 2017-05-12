22314bc912d61e1cac0fd293990ac9075e65e7a4
Marcin Wojcik <marcinwojcik987@gmail.com>
2017-05-12 15:41:28 +0200

    change file names

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "paczkolaby";
-$password = "paczkolaby";
+$username = "root";
+$password = "coderslab";
 $baseName = "paczkolab";
 
 $conn = new mysqli($servername, $username, $password, $baseName);
